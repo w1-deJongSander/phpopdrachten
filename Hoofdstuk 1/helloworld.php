5196344e72c246d87f10976ec9b229816b02a766
Sander de Jong <sander.dejong1@edu-kw1c.nl>
2020-02-14 11:52:00 +0100

    Opdracht 1.1

---
+++ Hoofdstuk 1/helloworld.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title><?php print "pagina 1"; ?></title>
+    </head>
+    <body><h1>
+        <?php
+        echo "Hello World";
+    ?>
+    </h1>
+
+    </body>
+</html>
+
